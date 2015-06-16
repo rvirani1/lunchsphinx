e9cac9e4a68a1a5a4d2c707186bdc4a10d87742f
Riaz Virani <rvirani@emcien.com>
2015-06-15 20:48:05 -0400

    Only load extra env keys if present

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,5 +1,7 @@
 secret_path = File.expand_path("config/application.yml")
 
-YAML::load_file(secret_path).each do |key, value|
-  ENV[key] = value unless ENV.key?(key)
+if File.exist? secret_path
+  YAML::load_file(secret_path).each do |key, value|
+    ENV[key] = value unless ENV.key?(key)
+  end
 end
