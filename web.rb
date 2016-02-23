4b0fdc72cdaf1978a956f75d93a01937daf742e0
Riaz Virani <riaz.n.virani@gmail.com>
2016-02-23 16:36:32 -0500

    Fix comments

--- web.rb
+++ web.rb
@@ -17,16 +17,24 @@ get '/' do
 end
 
 get '/randomrestaurant' do
-  #Set distance to 800 meters if distance is actually not a number. Assuming people walk 80 meters a minute
+  # Set distance to 800 meters if distance is actually not a number. Assuming people walk 80 meters a minute
+
   if request["distance"] == "NaN"
     distance = 800
   else
     distance = request["distance"].to_i * 80
   end
-  #get restaurant request object from google
+
+  # Get restaurant request object from google
   google = RestaurantRequest.new(ENV['GOOGLE_API_SERVER_KEY'])
-  restaurant = google.random_restaurant(latitude: request["latitude"], longitude: request["longitude"], distance: distance)
-  #redirect to index if there were no results
+  restaurant = google.random_restaurant(
+    latitude: request["latitude"],
+    longitude: request["longitude"],
+    distance: distance
+  )
+
+  # Redirect to index if there were no results
+
   unless restaurant
     session[:flash] = "There are no results in your area"
     redirect to('/')
