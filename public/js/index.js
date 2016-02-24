c667cf700952e565635f13a0edff28f103e03c31
Riaz Virani <riaz.n.virani@gmail.com>
2016-02-23 20:49:35 -0500

    Reformat some js code

--- public/js/index.js
+++ public/js/index.js
@@ -1,22 +1,18 @@
-$( document ).ready( function() {
-    console.log("in the javascript file");
-//    Clicking the submit button
-    $('#submit').click(function() {
-        //    Get location and show error message to user
-        distance = parseInt($("#distance").val());
-        console.log("Distance is " + distance);
-        navigator.geolocation.getCurrentPosition(GetLocation);
-        function GetLocation(loc_data) {
-            console.log("getting position");
-            latitude = loc_data.coords.latitude;
-            longitude = loc_data.coords.longitude;
-            console.log("latitude is " + latitude);
-            console.log("longitude is " + longitude);
-            console.log("accuracy is " + loc_data.coords.accuracy);
-            window.location = 'http://' + location.host + '/randomrestaurant?latitude=' + latitude + '&longitude=' + longitude + '&distance=' + distance;
-        }
-    }); // closing the click function
-
-
-
-} );
+$(document).ready(function () {
+  // Clicking the submit button
+  $('#submit').click(function () {
+    //    Get location and show error message to user
+    distance = parseInt($("#distance").val());
+    console.log("Distance is " + distance);
+    navigator.geolocation.getCurrentPosition(GetLocation);
+    function GetLocation(loc_data) {
+      console.log("getting position");
+      latitude = loc_data.coords.latitude;
+      longitude = loc_data.coords.longitude;
+      console.log("latitude is " + latitude);
+      console.log("longitude is " + longitude);
+      console.log("accuracy is " + loc_data.coords.accuracy);
+      window.location = 'http://' + location.host + '/randomrestaurant?latitude=' + latitude + '&longitude=' + longitude + '&distance=' + distance;
+    }
+  }); // closing the click function
+});
